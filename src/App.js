26d054d93228868d71367e690c86bf4afc6efc0d
Rui Yu <rui.yu@bouvet.no>
2019-02-17 12:21:10 +0100

    Add some redux files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './reducers';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Provider store={store}>
+        <div className="App" />
+      </Provider>
     );
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,23 @@
 // @flow
+
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import aqis from './aqis';
+import areas from './areas';
+import components from './components';
+import stations from './stations';
+
+const rootReducer = combineReducers({
+  aqis,
+  areas,
+  components,
+  stations
+});
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware))
+);
