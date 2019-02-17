2ce9edbdc1f5570f07ab3d2058782a68c3cf670f
Rui Yu <rui.yu@bouvet.no>
2019-02-17 11:55:28 +0100

    Add some base files

---
+++ src/axios.js
@@ -0,0 +1,5 @@
+// @flow
+
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.nilu.no/';
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
+
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
